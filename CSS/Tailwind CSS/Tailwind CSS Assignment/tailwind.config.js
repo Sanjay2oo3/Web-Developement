3c3c410295fc4c8d644937f88ee8bbcc2072bd87
Sanjay2oo3 <itssanjaychoudhary423@gmail.com>
2023-08-12 19:15:06 +0530

    Git Hub Assignment

---
+++ CSS/Tailwind CSS/Tailwind CSS Assignment/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Tailwind\ CSS/Tailwind\ CSS \ Assignment/**/*.html"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
